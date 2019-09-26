48a8ad25afb9c2af978ece181db5b3777caff757
MuHeDing <41604436+MuHeDing@users.noreply.github.com>
2019-09-26 18:38:16 +0800

    增加 and or

--- Inverted index/good.py
+++ Inverted index/good.py
@@ -1,18 +1,15 @@
-import os
 import re
-import ast
 import nltk
 def token_stream(line):
     # re.I 不区分大小写
     li=nltk.word_tokenize(line)
     li=' '.join(li)
-    return re.findall(r'\w+', li,re.I)
+    return re.findall(r'\w+', li,re.I)  #返回一个列表
 
 # 匹配
 def mapper(lineNum, list):
     dic = {}
     for item in list:
-        #print(item)
         key = ''.join([str(lineNum), ':', item])
         if key in dic.keys():
             ll = dic.get(key)
@@ -28,7 +25,7 @@ def reducer(dic):
     rdic = {}
     for key in keys:
         lineNum, kk = key.split(":")
-        ss = ''.join([lineNum, ':', str(dic.get(key))])
+        ss = ''.join([lineNum, ':', str(dic.get(key))]) #变成字符串
         if kk in rdic.keys():
             ll = rdic[kk]
             ll.append(ss)
@@ -37,7 +34,7 @@ def reducer(dic):
             rdic[kk] = [ss]
 
     return rdic
-# 结合
+# 结合 次数
 def combiner(dic):
     keys = dic.keys()
     tdic = {}
@@ -50,71 +47,107 @@ def combiner(dic):
         tdic[key] = count
     return tdic
 
-# def shuffle(dic):
-#     dict = sorted(dic.items(), key=lambda x: x[0])
-#     return dict
+def shuffle(dic):
+    dict = sorted(dic.items(), key=lambda x: x[0])
+    return dict
 
-# def get_reverse_index(filepath):
-#     file = open(filepath, 'r')
-#     lineNum = 0
-#     rdic_p = {}
-#     while True:
-#         lineNum += 1
-#         content = file.readlines() #读入一行，返回一个字符串
-#         #print(type(content))
-#         word = d['text']
-#         d = eval(t)
-#         word = d['text']
-#         if word != '':
-#             pass
-#         else:
-#             break
-#         list = token_stream(word)
-#         #print(list)
-#         mdic = mapper(lineNum, list)
-#        # print(mdic)
-#         cdic = combiner(mdic)
-#         #print(cdic)
-#         rdic_p.update(cdic) #把cdic的键值赋值给 rdic_p
-#
-#     rdic = reducer(rdic_p)
-#
-#     sdic = shuffle(rdic)
-#     return sdic
+
+def process(dic):
+    pdic={}
+    for word in dic.keys():
+        lis=dic[word]
+        for u in lis:
+            x,y=u.split(':')
+            if word in pdic.keys():
+                ll=pdic[word]
+                ll.append(x)
+                pdic[word] = ll
+            else:
+                pdic[word] =[x]
+    return pdic
+
+
 def get_reverse_index(filepath):
     file = open(filepath, 'r')
     lineNum = 0
     rdic_p = {}
     for content in file.readlines():
         lineNum += 1
-        d = eval(content)
+        d = eval(content) #变成字典
         word = d['text']
-        if word != '':
-            pass
-        else:
+        if word == '':
             break
         list = token_stream(word)
-      #  print(list)
         mdic = mapper(lineNum, list)
-       # print(mdic)
+        #print(mdic)
         cdic = combiner(mdic)
         #print(cdic)
         rdic_p.update(cdic) #把cdic的键值赋值给 rdic_p
 
     rdic = reducer(rdic_p)
 
-    #sdic = shuffle(rdic)
+    # sdic = shuffle(rdic)
+    # print(type(sdic))
     return rdic
 
 if __name__ == '__main__':
-    filepath="F:\\信息检索\\tweets.txt"
-    dic = get_reverse_index(filepath)
-    print(dic)
-    # 查询
+    filepath="F:\\信息检索\\tweetsFor test.txt"
+    rdic = get_reverse_index(filepath)
+    dic=process(rdic)
+   # print(dic)
+    print(rdic)
+    #print(dic)
+
     # while(1):
+    #     answer_set = set()
     #     search_word = input('Please input the word you want to search: ')
+    #     if 'or' in search_word or 'OR' in search_word:
+    #         Query =search_word.replace('or','',re.IGNORECASE)
+    #         Q_word_list = Query.split()
+    #         for word in Q_word_list:
+    #             if word in dic.keys():
+    #                 x=set(dic[word])
+    #                 answer_set =answer_set.union(x)  # set or
+    #         print(answer_set)
     #
-    #     if search_word in dic.keys():
-    #         print(dic.get(search_word))
+    #     elif 'and' in search_word or 'AND' in search_word:
+    #         Query =search_word.replace('and','',re.IGNORECASE)
+    #
+    #         Q_word_list = Query.split()
+    #         i=0
+    #         for word in Q_word_list:
+    #             if word in dic.keys() and i==0:
+    #                 x=set(dic[word])
+    #                 answer_set=x
+    #                 i = i + 1
+    #             elif word in dic.keys() and i==1:
+    #                 x = set(dic[word])
+    #                 answer_set =answer_set.intersection(x)  # set or
+    #
+    #         print(answer_set)
+    #     elif 'not' in search_word or 'NOT' in search_word:
+    #         Query =search_word.replace('not','',re.IGNORECASE)
+    #
+    #         Q_word_list = Query.split()
+    #         i=0
+    #         for word in Q_word_list:
+    #             if word in dic.keys() and i==0:
+    #                 x=set(dic[word])
+    #                 answer_set=x
+    #                 i = i + 1
+    #             elif word in dic.keys() and i==1:
+    #                 x = set(dic[word])
+    #                 answer_set.difference_update(x)
+    #
+    #         print(answer_set)
     #     else:
-    #         print(-1)
+    #         print(dic.get(search_word))
+    #
+
+# # 查询
+# while (1):
+#     search_word = input('Please input the word you want to search: ')
+#
+#
+#     else:
+#         print(-1)
